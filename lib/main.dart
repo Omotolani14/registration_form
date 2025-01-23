a91fbe73e8af48584c736431b37a9eac17ac4036
MARYAM <salisumaryam1401@gmail.com>
2025-01-23 03:39:45 -0800

    Registration Form

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'Screens/Registration_Screen.dart';
+
+void main (){
+  runApp(MyApp());
+}
+class MyApp extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+   return MaterialApp(
+    title: "Registration Form",
+    home:RegistrationScreen() ,
+
+   );
+  }
+}
