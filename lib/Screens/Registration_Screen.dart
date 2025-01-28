b18c7bc16fd910ecc664d8144e8ce21c0282ba2f
MARYAM <salisumaryam1401@gmail.com>
2025-01-28 04:30:27 -0800

    Registation_Screen

--- lib/Screens/Registration_Screen.dart
+++ lib/Screens/Registration_Screen.dart
@@ -1,6 +1,4 @@
 
-
-
 import 'package:flutter/material.dart';
 
 class RegistrationScreen extends StatefulWidget {
@@ -12,12 +10,62 @@ class _RegistrationScreenState extends State<RegistrationScreen>{
   final TextEditingController lastnameController= TextEditingController ();
   final TextEditingController emailController= TextEditingController ();
   bool isRegistered = false;
-}
+
  @override  
  Widget build(BuildContext context){
   return Scaffold(
     appBar: AppBar(
     title:Text("RegistrationForm"),
     ),
+    body:Padding(padding: EdgeInsets.all(20),
+    child: Column(
+      mainAxisAlignment:MainAxisAlignment.center ,
+      children: [
+      TextField(controller: firstnameController,
+      decoration: InputDecoration(labelText:"First Name"
+       ),
+      
+      ),
+      TextField(controller: lastnameController,
+      decoration: InputDecoration(labelText: "Last Name"
+      ),
+
+      ),
+      TextField(controller:emailController,
+      decoration: InputDecoration(labelText: "Email Address"
+      ),
+
+      ),
+      SizedBox(height:30,),
+      GestureDetector(
+        onTap: () {
+          setState(() {
+          isRegistered =!isRegistered;  
+          });
+        },
+        child: Container(
+          padding: EdgeInsets.all(20),
+          decoration: BoxDecoration(
+            color:isRegistered?Colors.greenAccent:Colors.blueAccent,
+            borderRadius: BorderRadius.circular(10),
+
+            
+          ),
+          child: Text(
+            isRegistered?"Registraton Sucessful":"Register me",
+            style:TextStyle(
+              color: Colors.black,
+              fontSize: 20,
+            )
+          ),
+          
+        
+          
+        ),
+      ),
+      ],
+    ),
+    ) ,
   );
  }
+}
